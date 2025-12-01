1855238ef2b757d28ac9088f5944e5c6b5e2ad63
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2025-12-01 18:26:43 +0300

    Day01 - solved

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -3,12 +3,46 @@ import java.io.File
 
 class Day01 : Base<List<Pair<Direction, Int>>, Int>(1) {
 
+    private val initPosition = 50
+
     override fun part1(input: List<Pair<Direction, Int>>): Int {
-        return 0
+        var position = initPosition
+        var result = 0
+
+        input.forEach { step ->
+            if (position == 0) result++
+            repeat(step.second) {
+                when (step.first) {
+                    Direction.LEFT -> position--
+                    Direction.RIGHT -> position++
+                    else -> Unit
+                }
+                if (position == -1) position = 99
+                if (position == 100) position = 0
+            }
+        }
+
+        return result
     }
 
     override fun part2(input: List<Pair<Direction, Int>>): Int {
-        return 0
+        var position = initPosition
+        var result = 0
+
+        input.forEach { step ->
+            repeat(step.second) {
+                if (position == 0) result++
+                when (step.first) {
+                    Direction.LEFT -> position--
+                    Direction.RIGHT -> position++
+                    else -> Unit
+                }
+                if (position == -1) position = 99
+                if (position == 100) position = 0
+            }
+        }
+
+        return result
     }
 
     override fun mapInputData(file: File): List<Pair<Direction, Int>> =
