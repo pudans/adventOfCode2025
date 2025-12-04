79eb71ff840050ba1df343682eddd434b11f7451
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2025-12-04 20:41:40 +0300

    Day04 - part 2

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,9 +1,14 @@
+import utils.Point
 import java.io.File
 
 class Day04 : Base<Matrix<Char>, Int>(4) {
 
     override fun part1(input: Matrix<Char>): Int {
-        var result = 0
+        return getAccessibleRolls(input).size
+    }
+
+    private fun getAccessibleRolls(input: Matrix<Char>): List<Point<Int>> {
+        val result = mutableListOf<Point<Int>>()
         for (i in input.indices) {
             for (j in input.first().indices) {
                 if (input[i][j] == '@') {
@@ -17,7 +22,7 @@ class Day04 : Base<Matrix<Char>, Int>(4) {
                     if (input.isRoll(i, j + 1)) d++
                     if (input.isRoll(i, j - 1)) d++
 
-                    if (d < 4) result++
+                    if (d < 4) result.add(Point(i, j))
                 }
             }
         }
@@ -28,7 +33,16 @@ class Day04 : Base<Matrix<Char>, Int>(4) {
         i >= 0 && j >= 0 && i < this.size && j < this.first().size && this[i][j] == '@'
 
     override fun part2(input: Matrix<Char>): Int {
-        return 0
+        var inLoop = true
+        var result = 0
+        val mutableInput = input.map { it.toMutableList() }.toMutableList()
+        while (inLoop) {
+            val rolls = getAccessibleRolls(mutableInput)
+            result += rolls.size
+            rolls.forEach { roll -> mutableInput[roll.x][roll.y] = '.' }
+            inLoop = rolls.isNotEmpty()
+        }
+        return result
     }
 
     override fun mapInputData(file: File): Matrix<Char> =
