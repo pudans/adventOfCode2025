48c305e14e935e437c5be0946cc032e44170e2a9
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2025-12-04 19:36:50 +0300

    Day03 - part 1

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,3 +1,4 @@
+import utils.println
 import java.io.File
 
 typealias Matrix<T> = List<List<T>>
@@ -5,7 +6,18 @@ typealias Matrix<T> = List<List<T>>
 class Day03 : Base<Matrix<Int>, Int>(3) {
 
     override fun part1(input: Matrix<Int>): Int {
-        return 0
+        return input.sumOf { foundMax(it) }
+    }
+
+    private fun foundMax(input: List<Int>): Int {
+        var result = 0
+        for (i in 0..<input.lastIndex) {
+            for (j in (i + 1)..input.lastIndex) {
+                val d = input[i] * 10 + input[j]
+                if (d > result) result = d
+            }
+        }
+        return result
     }
 
     override fun part2(input: Matrix<Int>): Int {
