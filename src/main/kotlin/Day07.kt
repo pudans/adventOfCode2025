a5176e54ea461eeb20a9721d34208acf1136b246
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2025-12-07 16:37:40 +0300

    Day07 - blank

---
+++ src/main/kotlin/Day07.kt
@@ -0,0 +1,43 @@
+import utils.PointX
+import java.io.File
+
+class Day07 : Base<Day07.Data, Int>(7) {
+
+    data class Data(
+        val start: PointX,
+        val splitter: List<PointX>,
+        val width: Int,
+        val height: Int
+    )
+
+    override fun part1(input: Data): Int {
+        return 0
+    }
+
+    override fun part2(input: Data): Int {
+        return 0
+    }
+
+    override fun mapInputData(file: File): Data {
+        val lines = file.readLines()
+        val startY = lines[0].indexOf('S')
+        val splitter = mutableListOf<PointX>()
+        for (j in lines.indices) {
+            for (i in lines[j].indices) {
+                if (lines[j][i] == '^') {
+                    splitter.add(PointX(j, i))
+                }
+            }
+        }
+        return Data(
+            start = PointX(0, startY),
+            width = lines[0].length,
+            height = lines.size,
+            splitter = splitter
+        )
+    }
+}
+
+fun main() {
+    Day07().submitAll()
+}
