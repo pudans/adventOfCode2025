3aab72c3b9b725b444eb4df9188e3f2a37f38ee9
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2025-12-09 09:24:04 +0300

    Day09 - part 1 - success

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,19 +1,26 @@
 import utils.Point
 import java.io.File
+import kotlin.math.abs
 
-class Day09 : Base<List<Point<Int>>, Long>(9) {
+class Day09 : Base<List<Point<Long>>, Long>(9) {
 
-    override fun part1(input: List<Point<Int>>): Long {
+    override fun part1(input: List<Point<Long>>): Long {
+        val areas = mutableListOf<Long>()
+        for (i in 0..<input.lastIndex) {
+            for (j in i + 1..input.lastIndex) {
+                areas.add(abs(input[i].x - input[j].x + 1) * abs(input[i].y - input[j].y + 1))
+            }
+        }
+        return areas.max()
+    }
+
+    override fun part2(input: List<Point<Long>>): Long {
         return 0L
     }
 
-    override fun part2(input: List<Point<Int>>): Long {
-        return 0L
-    }
-
-    override fun mapInputData(file: File): List<Point<Int>> =
+    override fun mapInputData(file: File): List<Point<Long>> =
         file.readLines().map { str ->
-            str.split(",").let { Point(it[0].toInt(), it[1].toInt()) }
+            str.split(",").let { Point(it[0].toLong(), it[1].toLong()) }
         }
 }
 
