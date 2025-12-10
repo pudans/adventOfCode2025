685318b66632cc83b1bea3e4c8fa10e4dbb48e5f
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2025-12-10 18:58:36 +0300

    Day10 - part 1 - failed

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,5 +1,9 @@
+import utils.PointX
 import utils.println
 import java.io.File
+import java.util.LinkedList
+import java.util.Queue
+import kotlin.math.min
 
 class Day10 : Base<List<Day10.Data>, Int>(10) {
 
@@ -9,9 +13,37 @@ class Day10 : Base<List<Day10.Data>, Int>(10) {
         val joltage: List<Int>
     )
 
-    override fun part1(input: List<Data>): Int {
-        println(input)
-        return 0
+    override fun part1(input: List<Data>): Int = input.sumOf { walk(it) }
+
+    val pass: Queue<Pair<Int, List<Boolean>>> = LinkedList()
+
+    private fun walk(data: Data): Int {
+        var result = 0
+        pass.add(Pair(0, data.target.map { false }))
+        while (pass.isNotEmpty()) {
+            val step = pass.remove()
+            if (pass.isNotEmpty() && step.second.all { false }) {
+                continue
+            }
+            if (step.first >= 10) {
+                continue
+            }
+            if (step.second == data.target) {
+                result = if (result == 0) step.first else min(result, step.first)
+                continue
+            }
+            if (result != 0 && step.first > result) {
+                continue
+            }
+//            println(step)
+
+            data.buttons.forEach { button ->
+                val new = step.second.mapIndexed { index, bool -> if (button.contains(index)) !bool else bool }
+                pass.add(step.first + 1 to new)
+            }
+        }
+        pass.clear()
+        return result
     }
 
     override fun part2(input: List<Data>): Int {
