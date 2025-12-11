67aee588e3a9d02cc04ba1ef0187f0ca7c56f7ff
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2025-12-11 18:01:20 +0300

    Day11 - part 1 - success

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -2,8 +2,29 @@ import java.io.File
 
 class Day11 : Base<Map<String, List<String>>, Int>(11) {
 
+    val memory = mutableMapOf<String, Int>()
+
     override fun part1(input: Map<String, List<String>>): Int {
-        return 0
+        return walk(input, "you", emptySet())
+    }
+
+    private fun walk(input: Map<String, List<String>>, point: String, path: Set<String>): Int {
+        if (memory.containsKey(point)) {
+            return memory[point]!!
+        }
+        if (point == "out") {
+            return 1
+        }
+        if (path.contains(point)) {
+            return 0
+        }
+        val newSet = HashSet(path).apply { add(point) }
+        val points = input[point]
+        return points!!.sumOf { point ->
+            val result = walk(input, point, newSet)
+            memory[point] = result
+            result
+        }
     }
 
     override fun part2(input: Map<String, List<String>>): Int {
