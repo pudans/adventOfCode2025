2aaf268a1c78d963e72e757b700a7e86020ade95
konstantin.karpyshev <konstantin.karpyshev@shopback.com>
2025-12-12 14:11:18 +0300

    Day12 - part 1 - failed

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.random.Random
 
 class Day12 : Base<Day12.Data, Int>(12) {
 
@@ -14,7 +15,55 @@ class Day12 : Base<Day12.Data, Int>(12) {
     )
 
     override fun part1(input: Data): Int {
-        return 0
+        return input.regions.count { fit(input.shapes, it) }
+    }
+
+    private fun fit(shapes: List<Matrix<Char>>, region: Region): Boolean {
+        val random = Random(System.nanoTime())
+        repeat(100) {
+            val rotations = shapes.map { random.nextInt(4) }
+            val positions  = shapes.map {
+                random.nextInt(1, region.width - 1) to (random.nextInt(1, region.height - 1))
+            }
+            val field = Array(region.width) { i -> IntArray(region.height) { 0} }
+            shapes.forEachIndexed { index, shape ->
+                val position = positions[index]
+                val rotated = rotate(rotations[index], shape)
+
+                if (rotated[0][0] == '#') field[position.first - 1][position.second - 1]
+                if (rotated[1][0] == '#') field[position.first - 1][position.second]
+                if (rotated[2][0] == '#') field[position.first - 1][position.second + 1]
+                if (rotated[0][1] == '#') field[position.first][position.second - 1]
+                if (rotated[1][1] == '#') field[position.first][position.second]
+                if (rotated[2][1] == '#') field[position.first][position.second + 1]
+                if (rotated[0][2] == '#') field[position.first + 1][position.second - 1]
+                if (rotated[1][2] == '#') field[position.first + 1][position.second]
+                if (rotated[2][2] == '#') field[position.first + 1][position.second + 1]
+            }
+            if (field.all { it.all { it <= 1} }) return true
+        }
+        return false
+    }
+
+    private fun rotate(rotation: Int, shape: Matrix<Char>): Matrix<Char> {
+        when (rotation) {
+            0 -> return shape
+            1 -> return listOf(
+                listOf(shape[2][0], shape[1][0], shape[0][0]),
+                listOf(shape[2][1], shape[1][1], shape[0][1]),
+                listOf(shape[2][2], shape[1][2], shape[0][2]),
+            )
+            2 -> return listOf(
+                listOf(shape[2][2], shape[2][1], shape[2][0]),
+                listOf(shape[1][2], shape[1][1], shape[1][0]),
+                listOf(shape[0][2], shape[0][1], shape[0][0]),
+            )
+            else -> return listOf(
+                listOf(shape[0][2], shape[1][2], shape[2][2]),
+                listOf(shape[0][1], shape[1][1], shape[2][1]),
+                listOf(shape[0][0], shape[1][0], shape[2][0]),
+            )
+        }
     }
 
     override fun part2(input: Data): Int {
@@ -59,5 +108,5 @@ class Day12 : Base<Day12.Data, Int>(12) {
 }
 
 fun main() {
-    Day12().submitAll()
+    Day12().submitPart1TestInput()
 }
